b3e57276795bc8bc311a46888954007ce8a4c690
Niels Onderbeke <niels.onderbeke@student.howest.be>
2021-10-23 13:15:42 +0200

    form accesiblity

---
+++ src/components/Icon.vue
@@ -0,0 +1,27 @@
+<script lang="ts">
+import { defineComponent } from "vue";
+import * as icons from "lucide-vue-next";
+
+export default defineComponent({
+  props: {
+    name: {
+      type: String,
+      required: true,
+    },
+  },
+  setup(props) {
+    const getIcon = (): any => {
+      // @ts-ignore
+      return icons[props.name];
+    };
+
+    return {
+      getIcon,
+    };
+  },
+});
+</script>
+
+<template>
+  <component :is="getIcon()" />
+</template>
--- src/screens/NewRoutine.vue
+++ src/screens/NewRoutine.vue
@@ -1,10 +1,12 @@
 <script lang="ts">
 import { defineComponent, ref } from "vue";
 import AppHeader from "../components/AppHeader.vue";
+import Icon from "../components/Icon.vue";
 
 export default defineComponent({
   components: {
     AppHeader,
+    Icon,
   },
   setup() {
     const colorOptions: any[] = [
@@ -15,8 +17,13 @@ export default defineComponent({
       { name: "pink", css: "bg-pink-300" },
     ];
 
+    const icons: any[] = [
+      "Activity", "Music", "Headphones", "Bell" ,"Calculator", "Contact", "Package", "Bitcoin", "Book"
+    ];
+
     return {
       colorOptions,
+      icons
     };
   },
 });
@@ -34,7 +41,7 @@ export default defineComponent({
       <label class="block mb-2 font-bold" for="title">Routing title</label>
       <input
         placeholder="eg. Morning routine"
-        class="border-2 border-gray-300 block rounded-md w-full p-2 mb-6"
+        class="border-2 border-gray-300 block rounded-md w-full p-2 mb-6 focus:outline-none focus:ring-2 ring-blue-400"
         type="text"
         name="title"
         id="title"
@@ -67,11 +74,11 @@ export default defineComponent({
       </div>
       <p class="mb-2 font-bold">Icon</p>
       <div class="grid grid-cols-5 mb-6 gap-2">
-        <label>
+        <label v-for="icon of icons" :key="icon">
           <input
             type="radio"
             name="icon"
-            id="music"
+            :id="icon"
             class="w-0 h-0 peer absolute overflow-hidden"
           />
           <div
@@ -87,30 +94,13 @@ export default defineComponent({
               place-items-center
             "
           >
-            <svg
-              class="w-6 h-6 stroke-current"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke-width="2"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-            >
-              <line x1="4" y1="21" x2="4" y2="14"></line>
-              <line x1="4" y1="10" x2="4" y2="3"></line>
-              <line x1="12" y1="21" x2="12" y2="12"></line>
-              <line x1="12" y1="8" x2="12" y2="3"></line>
-              <line x1="20" y1="21" x2="20" y2="16"></line>
-              <line x1="20" y1="12" x2="20" y2="3"></line>
-              <line x1="1" y1="14" x2="7" y2="14"></line>
-              <line x1="9" y1="8" x2="15" y2="8"></line>
-              <line x1="17" y1="16" x2="23" y2="16"></line>
-            </svg>
+            <Icon :name="icon"></Icon>
           </div>
         </label>
       </div>
+
       <input
-        class="w-full rounded-md p-2 font-bold bg-black text-white shadow-sm"
+        class="w-full rounded-md p-2 font-bold bg-black text-white shadow-sm focus:outline-none focus:ring-2 ring-blue-400"
         type="submit"
         value="Add routine"
       />
